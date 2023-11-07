9811492b1f6c8e0c9c064d4618fca2990740174d
rayray0708 <rachelmmm@192-168-1-110.tpgi.com.au>
2023-11-07 17:24:59 +0800

    added a popup to each marker and a legend on bottom-right corner, updated CSS file

--- Starter_Code/static/js/logic.js
+++ Starter_Code/static/js/logic.js
@@ -30,16 +30,12 @@ function addColour(depth){
     else return "#ffcc9b"
 };
 
-
-// function createMap() {
-// }
-
-
 //Define a function that creates a marker for each earthquake, 
 // earthquakes with higher magnitudes appear larger, 
 // and earthquakes with greater depth appear darker in colour.
 function createMarkers(response) {
     let earthquakes = response.features;
+    console.log(earthquakes);
 
     //Loop through each earthquake in the geoJson
     for (let index = 0; index < earthquakes.length; index++){
@@ -50,17 +46,19 @@ function createMarkers(response) {
         // coordinatesArray.push([earthquake.geometry.coordinates[1], earthquake.geometry.coordinates[0]]); 
 
         // L.cricle() is just a circle, L.circleMarker() has the radius parameter as well
-        L.circleMarker(coordinatesArray, {
+        let marker = L.circleMarker(coordinatesArray, {
             fillOpacity: earthquake.geometry.coordinates[2],
-            color: "white",
-            fillColor: addColour(earthquake.geometry.coordinates[2]),
-            radius: markerSize(earthquake.properties.mag)
+            color: "transparent", //set border colour to transparent
+            fillColor: addColour(earthquake.geometry.coordinates[2]), //the earthquake colour is based on its depth
+            radius: markerSize(earthquake.properties.mag) // the earthquake's marker size is based on it's magnitude
         //Add a pop-up to each marker
-        }).addTo(myMap);
+        })
+        marker.bindPopup("<h2> Magnitude: " + earthquake.properties.mag + "</h2> <hr> <h2> Location: " + earthquake.properties.place + "</h2> <hr> <h2> Depth: " + earthquake.geometry.coordinates[2] + "</h2>" );
+        marker.addTo(myMap);
     };
 };
 
-//Function to create legend
+// //Function to create legend
 let legend = L.control({position: "bottomright"}); 
 
 legend.onAdd = function () {
@@ -68,20 +66,23 @@ legend.onAdd = function () {
 
     let grid = [-10,10,30,50,70,90];
     
-    let colors = ["#98ee00",
-    "#d4ee00",
-    "#eecc00",
-    "#ee9c00",
-    "#ea822c",
-    "#ea2c2c"];
+    let colors = ["#ffcc9b",
+    "#ec988e",
+    "#dd828d",
+    "#c76b8e",
+    "#8e5b8f",
+    "#634d8c"];
 
     for (let i = 0; i < grid.length; i ++) {
         console.log(colors[i]);
 
         div.innerHTML += "<i style='background: " + colors[i] + "'></i> " + grid[i] + (grid[i + 1] ? "&ndash;" + grid[i + 1] + "<br>" : "+");
-
+        console.log(div.innerHTML);
     }
     return div; 
+    
 };
 
 legend.addTo(myMap);
+
+
